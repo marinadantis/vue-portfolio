85a0695b23108f80edca7e562627be2747bea803
Marina Dantis <marina.aqua@ymail.com>
2026-06-09 17:06:55 +0400

    chore: style for mobile designs

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,14 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { CONTACT_LINK } from '@/constants/navigation'
+</script>
 
 <template>
   <section
-    class="relative flex min-h-screen items-center justify-center md:pt-20 lg:pt-24 2xl:pt-0"
+    class="relative flex min-h-fit items-center justify-center py-24 md:min-h-screen md:py-0 md:pt-28 lg:pt-24 2xl:pt-0"
   >
     <div
-      class="mx-auto flex max-w-5xl flex-col items-center gap-8 p-4 lg:flex-row lg:gap-16 2xl:gap-20"
+      class="mx-auto flex max-w-5xl flex-col items-center gap-6 p-4 lg:flex-row lg:gap-16 2xl:gap-20"
     >
       <div
-        class="h-64 w-64 overflow-hidden rounded-t-full border-4 border-neutral-200/20 shadow-xl shadow-cyan-500/20 md:h-72 md:w-72 lg:h-80 lg:w-80 2xl:h-96 2xl:w-96"
+        class="h-48 w-48 overflow-hidden rounded-t-full border-4 border-neutral-200/20 shadow-xl shadow-cyan-500/20 md:h-72 md:w-72 lg:h-80 lg:w-80 2xl:h-96 2xl:w-96"
       >
         <img
           src="@/assets/images/about.jpg"
@@ -17,17 +19,25 @@
         />
       </div>
 
-      <div class="flex flex-col gap-8">
-        <h2 class="text-2xl font-black md:text-5xl lg:text-4xl 2xl:text-5xl">About me</h2>
+      <div class="flex flex-col gap-4 p-4 md:gap-6 md:text-left">
+        <h2
+          class="text-center text-3xl font-black md:text-left md:text-5xl lg:text-4xl 2xl:text-5xl"
+        >
+          About me
+        </h2>
 
-        <div class="flex flex-col gap-6">
-          <p class="max-w-2xl text-sm leading-relaxed text-neutral-400 lg:text-base 2xl:text-lg">
+        <div class="flex flex-col gap-4">
+          <p
+            class="max-w-xl text-sm leading-relaxed text-neutral-400 md:max-w-2xl lg:text-base 2xl:text-lg"
+          >
             I'm Marina, a web developer specializing in HTML, CSS, JavaScript, and modern frameworks
             like Vue.js and TailwindCSS. I enjoy building clean, scalable, and user-focused digital
             experiences that combine thoughtful design with reliable engineering.
           </p>
 
-          <p class="max-w-2xl text-sm leading-relaxed text-neutral-400 lg:text-base 2xl:text-lg">
+          <p
+            class="max-w-xl text-sm leading-relaxed text-neutral-400 md:max-w-2xl lg:text-base 2xl:text-lg"
+          >
             I work at the intersection of product and engineering - simplifying workflows, improving
             efficiency, and delivering features from ideation to production. From system design and
             implementation to iteration based on user feedback, I enjoy owning the full journey and
@@ -35,9 +45,9 @@
           </p>
         </div>
 
-        <div class="pt-3">
+        <div class="w-full pt-3 text-center md:text-left">
           <a
-            href="#contact"
+            :href="CONTACT_LINK.href"
             class="inline-flex transform items-center gap-1 rounded-xl bg-linear-to-r from-blue-500 via-cyan-400 to-teal-400 px-6 py-3 text-sm font-semibold text-white transition-all duration-100 ease-out hover:scale-105 active:scale-95"
           >
             <span> Contact me </span>
